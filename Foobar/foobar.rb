9ddef04dce1083846839e4dfb917baf9251cf936
Nadezhda <glinovanad@gmail.com>
2022-03-03 12:31:27 +0300

    done foobar

---
+++ Foobar/foobar.rb
@@ -0,0 +1,21 @@
+def foobar
+
+	# firstnum = gets.chomp.to_i
+	# secondnum = gets.chomp.to_i
+
+	firstnum = gets.chomp.to_f
+	secondnum = gets.chomp.to_f
+
+	if firstnum == 20
+		"так как первое число равно 20, вывожу второе значение: #{secondnum}"
+	elsif secondnum == 20 
+		"так как второе число равно 20, вывожу первое значение: #{firstnum}"
+	else sum = firstnum + secondnum
+		"сумма двух чисел равна #{sum}"
+	end
+
+end
+
+puts "ведите через Enter два числа"
+
+puts foobar
