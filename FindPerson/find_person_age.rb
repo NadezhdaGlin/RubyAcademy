5e940f71a9720a2fc06fa68e5f71a98fbe6070c9
Nadezhda <glinovanad@gmail.com>
2022-03-09 14:02:21 +0300

    searching by age and append to file

--- FindPerson/find_person_age.rb
+++ FindPerson/find_person_age.rb
@@ -11,22 +11,18 @@ puts "Введите возраст"
 age = gets.chomp.to_i
 
 flag = false
-file_data.each.with_index do |elem, index| 
+file_data.each.with_index do |elem, index|
   a = file_data[index].split
-  smth = a.each.map { |el| el}
-# puts smth.to_s
 
-if age == smth[-1].to_i
-  flag = true
-  # File.write("Text.txt",file_data[index], mode: "a")
-  puts "хорошо"
-else
-  puts "плохо"
-end 
+  if age == a[-1].to_i
+    flag = true
+    File.write("Text.txt",file_data[index]+"\n", mode: "a")
+  end 
 
 end
 
-
-# puts array.to_s
+if !flag
+  puts "такого возраста нет"
+end
 
 puts file_data.to_s
