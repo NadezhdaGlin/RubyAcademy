197a4cc1dd92d11e18d9f762eb372bdc9be24378
Nadezhda Glinova <79009458+NadezhdaGlin@users.noreply.github.com>
2022-04-02 22:11:05 +0300

    Update ending_word.rb

--- Wordwith_CS/ending_word.rb
+++ Wordwith_CS/ending_word.rb
@@ -1,16 +1,15 @@
 def word_ending_check
-
 	word = gets.chomp.to_s.strip
 	
-	arr = "#{word}".split(//)
+	arr = word.split(//)
 
 	if arr[-2..-1] == ["C","S"] 
-		2**("#{word}".length)
-	else "#{word}".reverse
+		2**(word.length)
+	else word.reverse
 	end
-
 end
 
 puts "Введите слово"
 
-puts word_ending_check
+word_ending_check
+
